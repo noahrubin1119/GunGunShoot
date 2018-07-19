334b6b4cb49724d94156e57539ee0bae0038a9bb
Noah Rubin <noahgidon@gmail.com>
2018-07-19 13:31:16 -0500

    Initial Commit

---
+++ Gun Gun ShootTests/Gun_Gun_ShootTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gun_Gun_ShootTests.swift
+//  Gun Gun ShootTests
+//
+//  Created by Noah Rubin on 7/19/18.
+//  Copyright © 2018 Noah Rubin. All rights reserved.
+//
+
+import XCTest
+@testable import Gun_Gun_Shoot
+
+class Gun_Gun_ShootTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
